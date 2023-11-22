eea40b8e342116c3abd6df3829f3f5e1146a7e30
Marco Lombardi <63810688+LombMarc@users.noreply.github.com>
2023-11-22 21:39:14 +0100

    Update continuous_deploy.sh

--- continuous_deploy.sh
+++ continuous_deploy.sh
@@ -4,17 +4,13 @@
 VIRTUAL_ENV="/.virtualenvs/venv"  # Adjust virtualenv name
 
 # Path to your Flask webapp in the GitHub repository
-APP_PATH="home/LombMarc/personal_finance_app"
-
-# Path to the WSGI configuration file
-WSGI_CONFIG="/var/www/username_pythonanywhere_com_wsgi.py"
+APP_PATH="home/LombMarc/personal_finance"
 
 # GitHub repository URL
 REPO_URL="https://github.com/LombMarc/personal_finance.git"  # Replace with your GitHub repo URL
 
 cd $APP_PATH
 git init
-git remote add origin $REPO_URL
 git pull origin main
 
 echo "deployment complete"
